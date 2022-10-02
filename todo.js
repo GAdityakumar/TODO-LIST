9c1b798f6bf162398cfc0a706a376227ff99264f
GAdityakumar <106055340+GAdityakumar@users.noreply.github.com>
2022-10-02 12:36:50 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,5 +1,7 @@
 document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
+    
+    
+    if(document.querySelector('#newtask input').value.length === 0){
         alert("Kindly Enter Task Name!!!!")
     }
 
@@ -9,12 +11,11 @@ document.querySelector('#push').onclick = function(){
                 <span id="taskname">
                     ${document.querySelector('#newtask input').value}
                 </span>
-                <button class="delete">delete
-                <i class="bi bi-trash3-fill"></i>
-                </button>
+                <button class="delete">delete</button>   
             </div>
-        `;
-
+        `
+        ;
+        document.querySelector('#newtask input').value = '';
         var current_tasks = document.querySelectorAll(".delete");
         for(var i=0; i<current_tasks.length; i++){
             current_tasks[i].onclick = function(){
@@ -23,3 +24,22 @@ document.querySelector('#push').onclick = function(){
         }
     }
 }
+function s(){
+    var i=document.getElementById("abc");
+    if(i.value=="")
+        {
+        document.getElementById("push").style.background = "#0d75ec";
+        document.getElementById("push").disabled=true;
+        
+        }
+      else if (i.value==0) {
+    document.getElementById("push").style.background = "#0d75ec";
+        document.getElementById("push").disabled=false;     
+        }
+      else{
+        document.getElementById("push").style.background = "green";
+        document.getElementById("push").disabled=false;  
+      }
+
+   }
+  
